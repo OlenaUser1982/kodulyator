41ab8cba94cc95889fbeaede4a06a7ffcbf91807
Iryna <i.mychkova@gmail.com>
2024-06-03 18:34:54 +0300

    -index +assets

--- copy-job.sh
+++ copy-job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Define source files and directories
-src_files=("dist/" "index.html" "style.css")
+src_files=("dist/" "style.css")
 
 # Define target directory
-target_dir="../iloc/wp-content/plugins/kodulyator/fe/"
+target_dir="../iloc/wp-content/plugins/kodulyator/assets/"
 
 # Loop through the source files and directories
 for src in "${src_files[@]}"; do
